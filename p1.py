a25bd016e25bc406c40d533663011f81a61e452e
Manjunatha KV <147262163+Manjunatha-kv01@users.noreply.github.com>
2026-02-20 07:54:06 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,8 @@
+# Python Program to check if a Number Is Positive Or Negative
+num = 15 # You can change this value to test with different numbers
+if num > 0:# Check if the number is greater than 0
+    print("Positive number") # If the number is greater than 0, it is a positive number
+elif num < 0: # Check if the number is less than 0
+    print("Negative number") # If the number is less than 0, it is a negative number
+else:# If the number is not greater than 0 and not less than 0, it must be zero
+    print("Zero") # If the number is neither greater than 0 nor less than 0, it is zero
